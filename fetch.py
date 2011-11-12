b1f5d85d3cefc8eec209347dcf1cef6ee9260755
xiaowen <xiaowenx@gmail.com>
2011-11-13 02:19:11 +0800

    Bug fix: delete old files correctly

--- fetch.py
+++ fetch.py
@@ -1,3 +1,8 @@
+#!/usr/bin/python
+
+# Download podcasts.
+# Add the following crontab entry: 0 * * * * root /home/xiaowen/code/music/fetch.py &> /dev/null
+
 from datetime import date, timedelta
 from pdb import set_trace as st
 import feedparser
@@ -5,11 +10,13 @@ import os
 import subprocess
 
 
+MUSIC_DIR = '/var/lib/mpd/music/'
+
 # Determine whether the given file is recent.
 def is_recent(name):
-    for d in range(7):
+    for d in range(3):
         dstr = (date.today() - timedelta(days=d)).strftime('%Y%m%d')
-        if dstr in link:
+        if dstr in name:
             return True
     return False
 
@@ -35,10 +42,10 @@ for source in sources:
         if not is_recent(link):
             continue
 
-        subprocess.call(['wget', '-c', '-P', '/var/lib/mpd/music/', link])
+        subprocess.call(['wget', '-c', '-P', MUSIC_DIR, link])
 
 # Delete old files.
-files = os.listdir('/var/lib/mpd/music/')
+files = os.listdir(MUSIC_DIR)
 for f in files:
     # We'll only deal with mp3 files.
     if not f.endswith('.mp3'):
@@ -46,7 +53,7 @@ for f in files:
 
     # If not recent, then delete.
     if not is_recent(f):
-        os.remove(f)
+        os.remove(os.path.join(MUSIC_DIR, f))
 
 # Load in mpd
 subprocess.call(['mpc', 'update'])
